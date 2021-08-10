93b8541694c5a9114807ebb3bdafdb5bcf19d6cd
Giovanni <gionneves@gmail.com>
2021-08-09 22:21:53 -0300

    first commit

---
+++ com/exercises/Um.java
@@ -0,0 +1,13 @@
+package com.exercises;
+/*
+Write a Java program to print 'Hello' on screen and then print your name on a separate line. Go to the editor
+Expected Output :
+Hello
+Alexandra Abramov
+*/
+
+public class Um {
+    public static void main(String[] args) {
+        System.out.println("Hello\nGiovanni Neves");
+    }
+}
